d5fa2c2231434044e534e07d25edd9a0993d0077
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-06-27 10:15:47 -0500

    Got initial files running

--- src/scripts/ContactList.js
+++ src/scripts/ContactList.js
@@ -2,22 +2,22 @@ const contactCollectionModule = require("./ContactCollection")
 
 const contactList = Object.create({}, {
   "buildContactList": {
-    value: function(){
+    value: function () {
       contactCollectionModule.getContacts()
-      .then((response) => {
-        console.log("all contacts", response)
-        const currentListRef = document.querySelector(".list-contacts-article")
-        if(currentListRef){
-          currentListRef.remove()
-        }
-        const contactsArticle = document.createElement("article")
-        contactsArticle.className = "list-contacts-article"
-        const contactModule = require("./Contact")
-        response.forEach(contact => {
-          contactsArticle.appendChild(contactModule.createContactComponent(contact))
-        });
-        document.querySelector("#display-container").appendChild(contactsArticle)
-      })
+        .then((response) => {
+          console.log("all contacts", response)
+          const currentListRef = document.querySelector(".list-contacts-article")
+          if (currentListRef) {
+            currentListRef.remove()
+          }
+          const contactsArticle = document.createElement("article")
+          contactsArticle.className = "list-contacts-article"
+          const contactModule = require("./Contact")
+          response.forEach(contact => {
+            contactsArticle.appendChild(contactModule.createContactComponent(contact))
+          });
+          document.querySelector("#display-container").appendChild(contactsArticle)
+        })
     }
   }
 })
